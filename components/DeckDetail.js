a11d48dc237e13e678df604e5a3d698f27e080dc
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-28 11:56:52 -0700

    add style individual card view

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import { getDeck } from '../utils/api';
+
 
 
 class DeckDetail extends React.Component {
   state = {
-    cardDeck: {}
+    title: '',
+    questions: []
   };
   componentDidMount() {
     getDeck(this.props.navigation.state.params.entryId).then(cardDeck => {
-      console.log(cardDeck);
+      const { title, questions } = JSON.parse(cardDeck);
       this.setState({
-        cardDeck
+        title,
+        questions
       });
     });
   }
 
   render() {
-    return(
-      <View>
-        <Text>Welcome to the page! {this.props.navigation.state.params.entryId}</Text>
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center'
+        }}
+      >
+        <Card title={this.state.title} >
+          <Text style={{marginBottom: 10, textAlign: 'center'}}>
+            {this.state.questions.length} cards
+          </Text>
+          <View>
+            <Button
+              icon={{name: 'add-circle'}}
+              backgroundColor='#03A9F4'
+              buttonStyle={styles.buttonStyle}
+              title='Add Card' />
+          </View>
+          <View>
+            <Button
+              icon={{name: 'play-arrow'}}
+              backgroundColor='#96C051'
+              buttonStyle={[styles.buttonStyle, { marginTop: 10 }]}
+              title='Start Quiz' />
+          </View>
+        </Card>
       </View>
-    );
+    )
   }
 }
+
+const styles = {
+  buttonStyle: {
+    borderRadius: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  }
+};
 
 export default DeckDetail;
