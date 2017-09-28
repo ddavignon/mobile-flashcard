4782bee5a43c5a9d5dc9e83412353153933667dc
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-27 22:30:39 -0700

    add input and button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FormLabel,
+  FormInput,
+  FormValidationMessage
+} from 'react-native-elements'
 
 
 export default class AddEntry extends React.Component {
+  state = {
+    titleText: '',
+    errorMessage: false
+  };
+
+  handleSubmit = () => {
+    console.log("this is" + this.state.titleText);
+    if (this.state.titleText) {
+
+      this.setState({
+        errorMessage: false,
+        titleText: ''
+      })
+
+    } else {
+      this.setState({ errorMessage: true })
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>What is the title of your new Deck?</Text>
+        <FormLabel>What is the title of your new Deck?</FormLabel>
+        <FormInput
+          onChangeText={titleText => this.setState({ titleText })}
+          value={this.state.titleText}
+        />
+        <FormValidationMessage>
+          {this.state.errorMessage ? 'This field is required': ''}
+        </FormValidationMessage>
+        <Button
+          title="Submit"
+          raised
+          backgroundColor="rgb(72, 149, 236)"
+          onPress={this.handleSubmit}
+        />
       </View>
     );
   }
