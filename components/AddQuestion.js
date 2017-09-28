6b667183c1bf94dd9c3b7843cacb9336c7f1c4e3
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-28 12:01:00 -0700

    add new component for questions form

---
+++ components/AddQuestion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+class AddEntry extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Add a questiion goes here
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default AddEntry;
