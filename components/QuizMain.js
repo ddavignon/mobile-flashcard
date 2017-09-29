f57112395be00c7b3a5c2debcf187dbd6efa68f6
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-28 19:26:15 -0700

    add feature flip card

--- components/QuizMain.js
+++ components/QuizMain.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import {
+  Badge,
   Button,
   Card
 } from 'react-native-elements';
 
 
 class QuizMain extends React.Component {
+  state = {
+    showQuestion: true
+  };
+
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.state.params.navTitle
@@ -15,11 +20,23 @@ class QuizMain extends React.Component {
 
   renderCard() {
     return (
-      <Card title="Quiz Question Here">
+      <Card
+        title={this.state.showQuestion ? "Quiz question" : "Quiz answer"}
+      >
+        <View style={styles.badgeStyle}>
+          <Badge
+            containerStyle={{ backgroundColor: 'violet'}}
+            onPress={() => this.setState({ showQuestion: !this.state.showQuestion })}
+          >
+            <Text>
+              {this.state.showQuestion ? "Answer" : "Question"}
+            </Text>
+          </Badge>
+        </View>
         <Button
           buttonStyle={styles.buttonStyle}
-          backgroundColor='#377D22'
           title="Correct"
+          backgroundColor='#377D22'
         />
         <Button
           buttonStyle={[styles.buttonStyle, { marginTop: 10 }]}
@@ -51,6 +68,12 @@ const styles = {
     marginLeft: 0,
     marginRight: 0,
     marginBottom: 0,
+  },
+  badgeStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
+    marginTop: 0
   }
 };
 
